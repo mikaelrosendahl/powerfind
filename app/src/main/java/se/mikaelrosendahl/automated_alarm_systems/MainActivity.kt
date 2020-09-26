e242cc715109de4bbd741bbe7887652dca6c8e5c
mikaelrosendahl <mikaelrosendahl76@gmail.com>
2020-09-26 21:20:04 +0200

    skapar constraintfragment

--- app/src/main/java/se/mikaelrosendahl/automated_alarm_systems/MainActivity.kt
+++ app/src/main/java/se/mikaelrosendahl/automated_alarm_systems/MainActivity.kt
@@ -5,7 +5,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.fragment_start_.*
+
 import se.mikaelrosendahl.automated_alarm_systems.ui.login.LoginFragment
 
 class MainActivity : AppCompatActivity() {
@@ -13,9 +13,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        okNextBtn.setOnClickListener {
-            supportFragmentManager.beginTransaction().replace(R.id.btnFragment, LoginFragment())
-        }
+
 
     }
 }
