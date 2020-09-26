2cadb0aaf30a78c2b3c740298512b1417f839772
mikaelrosendahl <mikaelrosendahl76@gmail.com>
2020-09-26 15:01:18 +0200

    Lägger till Start_Fragment.kt och skalar ner den.

---
+++ app/src/main/java/se/mikaelrosendahl/automated_alarm_systems/Start_Fragment.kt
@@ -0,0 +1,27 @@
+package se.mikaelrosendahl.automated_alarm_systems
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class Start_Fragment : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_start_, container, false)
+    }
+
+    
+}
