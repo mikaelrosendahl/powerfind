693a3af48b138a6866e1683e471e5963006efdde
mikaelrosendahl <mikaelrosendahl76@gmail.com>
2020-09-26 15:58:11 +0200

    lägger till fun onStart med setOnClickListener

--- app/src/main/java/se/mikaelrosendahl/automated_alarm_systems/Start_Fragment.kt
+++ app/src/main/java/se/mikaelrosendahl/automated_alarm_systems/Start_Fragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_start_.*
 
 
 class Start_Fragment : Fragment() {
@@ -21,6 +22,14 @@ class Start_Fragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_start_, container, false)
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        okNextBtn.setOnClickListener {
+            
+        }
     }
 
 
