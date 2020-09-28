b29591ca8ac94ac2c82d83e77db5727f0b0d4aea
mikaelrosendahl <mikaelrosendahl76@gmail.com>
2020-09-28 18:18:18 +0200

    Lagt till början till func på registerBtn

--- app/src/main/java/se/mikaelrosendahl/automated_alarm_systems/loginFragment.kt
+++ app/src/main/java/se/mikaelrosendahl/automated_alarm_systems/loginFragment.kt
@@ -1,13 +1,16 @@
 package se.mikaelrosendahl.automated_alarm_systems
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.fragment_login.*
 
 
 class loginFragment : Fragment() {
@@ -17,8 +20,35 @@ class loginFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         // Initialize Firebase Auth
         auth = Firebase.auth
+
+        // create fun for btn
+        loginRegBtn.setOnClickListener {
+            auth.createUserWithEmailAndPassword(loginEmailET.text.toString(),passwordET.text.toString() )
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d("mindebug", "createUserWithEmail:success")
+
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w("mindebug", "createUserWithEmail:failure", task.exception)
+                        Toast.makeText(baseContext, "Authentication failed.",
+                            Toast.LENGTH_SHORT).show()
+
+                    }
+
+                    // ...
+                }
+
+        }
+        loginBtn.setOnClickListener {
+
+
+        }
+
         }
 
     override fun onCreateView(
@@ -31,6 +61,13 @@ class loginFragment : Fragment() {
 
     override fun onStart() {
         super.onStart()
+
+        if(auth.currentUser == null)
+        {
+        
+
+        }
+
     }
 }
 
