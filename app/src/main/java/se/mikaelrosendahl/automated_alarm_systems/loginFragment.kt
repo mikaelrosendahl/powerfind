d3993c081a8af4fee82c06078f1483f41257b7d1
mikaelrosendahl <mikaelrosendahl76@gmail.com>
2020-09-28 19:06:17 +0200

    skapar variabel som tillåter activity i fragment

--- app/src/main/java/se/mikaelrosendahl/automated_alarm_systems/loginFragment.kt
+++ app/src/main/java/se/mikaelrosendahl/automated_alarm_systems/loginFragment.kt
@@ -1,5 +1,6 @@
 package se.mikaelrosendahl.automated_alarm_systems
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -39,8 +40,6 @@ class loginFragment : Fragment() {
                             Toast.LENGTH_SHORT).show()
 
                     }
-
-                    // ...
                 }
 
         }
@@ -64,7 +63,9 @@ class loginFragment : Fragment() {
 
         if(auth.currentUser == null)
         {
-        
+            val intent = Intent (this@loginFragment.context, LoginFrag::class.java)
+            startActivity(intent)
+
 
         }
 
