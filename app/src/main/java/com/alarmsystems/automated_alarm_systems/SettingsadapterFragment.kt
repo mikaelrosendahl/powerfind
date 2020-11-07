6d4cde885b1f60f42b24b2eedfcfaeb60535c371
mikael <mikaelrosendahll76@gmail.com>
2020-11-07 18:54:53 +0100

    Lägger till recyclern

--- app/src/main/java/com/alarmsystems/automated_alarm_systems/SettingsadapterFragment.kt
+++ app/src/main/java/com/alarmsystems/automated_alarm_systems/SettingsadapterFragment.kt
@@ -10,12 +10,15 @@ import android.view.ViewGroup
 class SettingsadapterFragment : Fragment() {
 
 
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.settingsadapterfragment, container, false)
+
+
     }
 
 }
