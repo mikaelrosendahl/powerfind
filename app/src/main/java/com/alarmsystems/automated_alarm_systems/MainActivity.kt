5e5fde76484189e075766d6907bdd0bfc7dc8fa5
mikael <mikaelrosendahll76@gmail.com>
2020-11-07 19:49:41 +0100

    Byter namn på class och variabel SettingsAdapter.kt

--- app/src/main/java/com/alarmsystems/automated_alarm_systems/MainActivity.kt
+++ app/src/main/java/com/alarmsystems/automated_alarm_systems/MainActivity.kt
@@ -4,23 +4,29 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import androidx.navigation.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
-
+import kotlinx.android.synthetic.main.settingsadapterfragment.*
 
 
 //Firebase instance
 private lateinit var auth: FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
-
-    var adapter = Myadapter()
+    //Creates instance of adapter
+    var theadapter = Settingsadapter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //Create ref ref to aadapter with layout
+        theRecView.layoutManager = LinearLayoutManager(this)
+        theRecView.adapter = theadapter
+
+        //ref to firebase auth
         auth = Firebase.auth
     }
     public override fun onStart() {
--- app/src/main/java/com/alarmsystems/automated_alarm_systems/SettingsAdapter.kt
+++ app/src/main/java/com/alarmsystems/automated_alarm_systems/SettingsAdapter.kt
@@ -5,7 +5,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
-class Myadapter () : RecyclerView.Adapter<MyViewHolder>(){
+class Settingsadapter () : RecyclerView.Adapter<MyViewHolder>(){
 
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
