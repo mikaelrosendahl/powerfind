5c31554ca7efdfba72597f4deefbee7db7845a11
mikael <mikaelrosendahll76@gmail.com>
2020-11-07 17:22:38 +0100

    Skapar settingsfrag.

---
+++ app/src/main/java/com/alarmsystems/automated_alarm_systems/SettingsadapterFragment.kt
@@ -0,0 +1,21 @@
+package com.alarmsystems.automated_alarm_systems
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class SettingsadapterFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_settingsadapter, container, false)
+    }
+
+}
