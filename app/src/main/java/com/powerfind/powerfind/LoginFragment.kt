2f7bca6a9f088c9b5edd6ff31f9740f8e99c9aaf
mikael <mikaelrosendahll76@gmail.com>
2020-11-30 16:12:53 +0100

    onView tillagt

--- app/src/main/java/com/powerfind/powerfind/LoginFragment.kt
+++ app/src/main/java/com/powerfind/powerfind/LoginFragment.kt
@@ -1,8 +1,10 @@
 package com.powerfind.powerfind
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import androidx.fragment.app.Fragment
 import android.view.View
+import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
@@ -13,6 +15,14 @@ import kotlinx.android.synthetic.main.fragment_login.*
 class LoginFragment : Fragment() {
 
     var auth: FirebaseAuth = Firebase.auth
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_login, container, false)
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 
