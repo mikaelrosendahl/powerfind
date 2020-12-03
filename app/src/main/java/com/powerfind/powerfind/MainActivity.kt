5f4e9f8962955211f02d09e78447d057528cc0ba
mikael <mikaelrosendahll76@gmail.com>
2020-12-03 12:09:13 +0100

    adderat kod för att skriva till och läsa från databasen

--- app/src/main/java/com/powerfind/powerfind/MainActivity.kt
+++ app/src/main/java/com/powerfind/powerfind/MainActivity.kt
@@ -8,8 +8,12 @@ import androidx.navigation.findNavController
 
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
+import com.google.firebase.database.ktx.getValue
 import com.google.firebase.ktx.Firebase
 import com.powerfind.powerfind.R
 
@@ -20,12 +24,34 @@ import com.powerfind.powerfind.R
 class MainActivity : AppCompatActivity() {
 
 
-
+    private lateinit var database: DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        database = Firebase.database.reference
+
+        val database = Firebase.database
+        val myRef = database.getReference("message")
+
+        myRef.setValue("Hello, World!")
+
+    // Read from the database
+    myRef.addValueEventListener(object : ValueEventListener {
+        override fun onDataChange(dataSnapshot: DataSnapshot) {
+          // This method is called once with the initial value and again
+          // whenever data at this location is updated.
+          val value = dataSnapshot.getValue<String>()
+          Log.d("debug", "Value is: $value")
+
+      }
+
+      override fun onCancelled(error: DatabaseError) {
+          // Failed to read value
+          Log.w("debug", "Failed to read value.", error.toException())
+      }
+     })
     }
 
 
--- app/src/main/java/com/powerfind/powerfind/MapsFragment.kt
+++ app/src/main/java/com/powerfind/powerfind/MapsFragment.kt
@@ -24,24 +24,6 @@ import com.google.firebase.ktx.Firebase
 
 class MapsFragment : Fragment() {
 
-    /*private lateinit var database: DatabaseReference
-
-    // Read from the database
-    myRef.addValueEventListener(object : ValueEventListener {
-        override fun onDataChange(dataSnapshot: DataSnapshot) {
-            // This method is called once with the initial value and again
-            // whenever data at this location is updated.
-            val value = dataSnapshot.getValue<String>()
-            Log.d(TAG, "Value is: $value")
-
-        }
-
-        override fun onCancelled(error: DatabaseError) {
-            // Failed to read value
-            Log.w(TAG, "Failed to read value.", error.toException())
-        }
-    })*/
-
     private val callback = OnMapReadyCallback { googleMap ->
 
         val uppsala = LatLng(59.8, 17.6)
