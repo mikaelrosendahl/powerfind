a5dc6411110ec12a40898b0ba797ccfe32eb8601
mikael <mikaelrosendahll76@gmail.com>
2020-12-05 12:19:54 +0100

    Tagit bort gamla importer och skapat cluster manager.

--- app/src/main/java/com/powerfind/powerfind/MapsFragment.kt
+++ app/src/main/java/com/powerfind/powerfind/MapsFragment.kt
@@ -1,29 +1,23 @@
 package com.powerfind.powerfind
 
-import android.content.ContentValues.TAG
-import androidx.fragment.app.Fragment
 
+import androidx.fragment.app.Fragment
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.ValueEventListener
-import com.google.firebase.database.ktx.database
-import com.google.firebase.database.ktx.getValue
-import com.google.firebase.ktx.Firebase
+
+import com.google.maps.android.clustering.ClusterManager
 
 class MapsFragment : Fragment() {
 
+    // Declare a variable for the cluster manager.
+    private lateinit var clusterManager: ClusterManager<MyItem>
     private val callback = OnMapReadyCallback { googleMap ->
 
         val uppsala = LatLng(59.8, 17.6)
