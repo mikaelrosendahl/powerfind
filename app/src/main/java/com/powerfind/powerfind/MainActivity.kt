6d0be70235f046816f9fc89234fc07da93ec6d6e
mikael <mikaelrosendahll76@gmail.com>
2020-12-06 13:05:10 +0100

    provar klassen MyItem.kt

--- app/src/main/java/com/powerfind/powerfind/MainActivity.kt
+++ app/src/main/java/com/powerfind/powerfind/MainActivity.kt
@@ -4,10 +4,6 @@ package com.powerfind.powerfind
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import androidx.navigation.findNavController
-
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
@@ -15,14 +11,11 @@ import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
 import com.google.firebase.database.ktx.getValue
 import com.google.firebase.ktx.Firebase
-import com.powerfind.powerfind.R
-
-
-
+import kotlinx.serialization.descriptors.StructureKind
+import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
-
 
     private lateinit var database: DatabaseReference
 
@@ -32,33 +25,29 @@ class MainActivity : AppCompatActivity() {
 
         database = Firebase.database.reference
 
-        val database = Firebase.database
-        val myRef = database.getReference("message")
+            // Set path to the database
+              val database = Firebase.database
+              val myRef = database.getReference("chargerstations")
 
-        myRef.setValue("Hello, World!")
 
-    // Read from the database
-    myRef.addValueEventListener(object : ValueEventListener {
-        override fun onDataChange(dataSnapshot: DataSnapshot) {
-          // This method is called once with the initial value and again
-          // whenever data at this location is updated.
-          val value = dataSnapshot.getValue<String>()
-          Log.d("debug", "Value is: $value")
-
-      }
-
-      override fun onCancelled(error: DatabaseError) {
-          // Failed to read value
-          Log.w("debug", "Failed to read value.", error.toException())
-      }
-     })
+              // Read from the database
+              myRef.addValueEventListener(object : ValueEventListener {
+                  override fun onDataChange(dataSnapshot: DataSnapshot) {
+                      // This method is called once with the initial value and again
+                      // whenever data at this location is updated.
+                      val value = dataSnapshot.getValue<MyItem>()
+                      Log.d("Debug", "Value is: $value")
+                  }
+                  override fun onCancelled(error: DatabaseError) {
+                      // Failed to read value
+                      Log.w("Debug", "Failed to read value.", error.toException())
+                  }
+              })
     }
-
 
     public override fun onStart() {
         super.onStart()
-        // Check if user is signed in (non-null) and update UI accordingly.
-
 
     }
+
 }
