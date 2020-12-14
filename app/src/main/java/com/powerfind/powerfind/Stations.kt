3f9aaf727d379e01c51dab374e193198b86d6308
mikael <mikaelrosendahll76@gmail.com>
2020-12-14 15:14:35 +0100

    lade tillbaka variabler

--- app/src/main/java/com/powerfind/powerfind/Stations.kt
+++ app/src/main/java/com/powerfind/powerfind/Stations.kt
@@ -5,4 +5,7 @@ import com.google.firebase.database.IgnoreExtraProperties
 @IgnoreExtraProperties
 data class Stations(
     var station: String = "chargerstations",
+    var number: Int? = 1410,
+    var att: String? = "csmd",
+
 )
